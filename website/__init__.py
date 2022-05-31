aaf87276606758c0fc198959ac8a76f05b188d71
paka95 <patryk.pomierski@gmail.com>
2022-05-31 12:16:48 +0200

    Corrected files after unsuccessful EB introduction

--- website/__init__.py
+++ website/__init__.py
@@ -1,56 +1,5 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from os import path
-# from flask_login import LoginManager
-# import os
-# from dotenv import load_dotenv
 
-# load_dotenv()
-
-# MY_ENV_VAR = os.getenv('MY_ENV_VAR')
-
-# db = SQLAlchemy()
-# DB_NAME = "database.db"
-
-# db_pass = os.getenv('db_user')
-# db_user = os.getenv('db_pass')
-
-# def create_app():
-#     app = Flask(__name__)
-#     app.config['SECRET_KEY'] = "secret"
-#     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_pass}@localhost/fiszki'
-#     db.init_app(app)
-
-#     # from .views import views
-#     from .auth import auth
-
-    
-
-#     # app.register_blueprint(views, url_prefix="/")
-#     app.register_blueprint(auth, url_prefix="/")
-
-#     # from .models import User, Post, Comment, Like
-
-#     create_database(app)
-
-
-#     login_manager = LoginManager()
-#     login_manager.login_view = "auth.login"
-#     login_manager.init_app(app)
-
-#     # @login_manager.user_loader
-#     # def load_user(id):
-#     #     return User.query.get(int(id))
-
-#     return app
-
-# def create_database(app):
-#     if not path.exists("website/" + DB_NAME):
-#         db.create_all(app=app)
-#         print("Created database")
-
-
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
@@ -60,33 +9,33 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = "secret"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:paka@localhost/fiszki'
-    db.init_app(app)
+    application = Flask(__name__)
+    application.config['SECRET_KEY'] = "secret"
+    application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:paka@localhost/fiszki'
+    db.init_app(application)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
+    application.register_blueprint(views, url_prefix="/")
+    application.register_blueprint(auth, url_prefix="/")
 
     from .models import User
 
-    create_database(app)
+    create_database(application)
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
-    login_manager.init_app(app)
+    login_manager.init_app(application)
     
 
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
 
-    return app
+    return application
 
-def create_database(app):
+def create_database(application):
     if not path.exists("website/" + DB_NAME):
-        db.create_all(app=app)
+        db.create_all(app=application)
         print("Database created")
